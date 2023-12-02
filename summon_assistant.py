7b1a633e0ebf8ad4882fb81ae2228a1395c4740b
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2023-12-02 11:59:47 -0500

    WIP: code
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- summon_assistant.py
+++ summon_assistant.py
@@ -9,53 +9,58 @@ from rich.progress import Progress
 import get_configs as config
 from utils import print_in_color
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
 
 class Assistant:
+
     def __init__(self, assistant_name, file):
         self.assistant_name = assistant_name
         self.file = file
-        self.file = client.files.create(
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+        self.file = self.client.files.create(
             file=open(self.file, "rb"),
             purpose='assistants'
         )
-        self.assistant = client.beta.assistants.create(
+        self.assistant = self.client.beta.assistants.create(
             name=self.assistant_name,
             instructions=config.get_openai_assistant_instructions(),
             tools=[{"type": "code_interpreter"}, {"type": "retrieval"}],
-            model="gpt-4-1106-preview",
+            model=config.get_openai_assistant_model_name(),
             file_ids=[self.file.id],
         )
 
+    def create_a_thread(self):
+        return self.client.beta.threads.create()
 
-def create_a_thread():
-    return client.beta.threads.create()
+    def add_message_to_thread(self, thread_id, content):
+        return self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=content,
+        )
 
+    def start_run(self, thread_id, assistant_id):
+        return self.client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+        )
 
-def add_message_to_thread(thread_id, content):
-    return client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=content,
-    )
+    def get_messages_and_print(self, thread_id, run_id):
+        messages = self.client.beta.threads.messages.list(
+            thread_id=thread_id,
+        )
 
+        for message in reversed(messages.data[:-1]):
+            if message.role != "user" and message.run_id == run_id:
+                print_in_color("Assistant: " + message.content[-1].text.value, config.get_info_color())
 
-def start_run(thread_id, assistant_id):
-    return client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant_id,
-    )
+    def delete_assistant(self):
+        if config.get_openai_delete_assistant_at_exit():
+            self.client.beta.assistants.delete(self.assistant.id)
+            self.client.files.delete(self.file.id)
 
-
-def get_messages_and_print(thread_id, run_id):
-    messages = client.beta.threads.messages.list(
-        thread_id=thread_id,
-    )
-
-    for message in reversed(messages.data[:-1]):
-        if message.role != "user" and message.run_id == run_id:
-            print_in_color("Assistant: " + message.content[-1].text.value, config.get_info_color())
+        message = "Assistant and files have been deleted successfully." if config.get_openai_delete_assistant_at_exit() else "Assistant and files have not been deleted."
+        print_in_color(message, config.get_warning_color())
 
 
 def summon_assistant():
@@ -65,7 +70,7 @@ def summon_assistant():
     assistant = Assistant(assistant_name, file)
     get_user_question = input("Ask `Kel`: ")
 
-    thread = create_a_thread()
+    thread = assistant.create_a_thread()
     # print(f"Thread id: {thread.id}")
 
     while get_user_question != ":q" or get_user_question != ":quit":
@@ -73,10 +78,10 @@ def summon_assistant():
             print_in_color("Exiting chat mode", config.get_info_color())
             break
 
-        message = add_message_to_thread(thread.id, get_user_question)
+        message = assistant.add_message_to_thread(thread.id, get_user_question)
         # print(f"Message id: {message.id}")
 
-        run = start_run(thread.id, assistant.assistant.id)
+        run = assistant.start_run(thread.id, assistant.assistant.id)
         # print(f"Run id: {run.id} | Run status: {run.status}")
 
         with Progress(transient=True) as progress:
@@ -84,7 +89,7 @@ def summon_assistant():
 
             while not progress.finished:
                 while run.status != "completed":
-                    run = client.beta.threads.runs.retrieve(
+                    run = assistant.client.beta.threads.runs.retrieve(
                         thread_id=thread.id,
                         run_id=run.id
                     )
@@ -94,5 +99,8 @@ def summon_assistant():
                         break
                     progress.update(task, advance=1)
 
-        get_messages_and_print(thread.id, run.id)
+        assistant.get_messages_and_print(thread.id, run.id)
+
         get_user_question = input("Ask `Kel`: ")
+
+    assistant.delete_assistant()

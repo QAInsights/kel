5cefd5588e1cfb9b2e6e427cc28e5d23fd600e8a
Eldad Uzman <eldad.uzman@merck.com>
2023-12-11 09:10:29 +0200

    move all sources to src dir

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-with open('requirements.txt') as f:
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-from setuptools import setup
 
 setup(
-    name='kel',
-    version='0.0.1',
-    packages=['app', 'app.gpt', 'app.utils', 'app.config', 'app.inputs', 'app.assistant', 'app.constants'],
-    url='https://kel.qainsights.com',
-    license='MIT',
-    author='NaveenKumar Namachivayam',
-    data_files=[('', ['requirements.txt', 'config.toml'])],
-    author_email='',
-    description='AI assistant in your CLI.',
+    name="kel",
+    version="0.0.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    url="https://kel.qainsights.com",
+    license="MIT",
+    author="NaveenKumar Namachivayam",
+    data_files=[("", ["requirements.txt", "config.toml"])],
+    author_email="",
+    description="AI assistant in your CLI.",
     install_requires=required,
 )

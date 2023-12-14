27788c57c882a7289a882f536cdef2d3ff3fcd3e
Eldad Uzman <eldad.uzman@merck.com>
2023-12-14 17:43:54 +0200

    move to poetry

--- src/kel/__main__.py
+++ src/kel/__main__.py
@@ -6,7 +6,6 @@ from kel.gpt.askgpt import gpt
 from kel.config.get_configs import get_enable_openai_assistant
 from kel.assistant.summon_assistant import summon_assistant
 
-
 def main():
     if get_enable_openai_assistant():
         summon_assistant()

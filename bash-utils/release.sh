229fdbb3cbbe7d74330fc0875bc5a751b1fb9ac5
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2024-01-02 21:49:21 -0500

    Fix: Config location
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- bash-utils/release.sh
+++ bash-utils/release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# IMPORTANT:
+# Change the version in pyproject.toml and __version__.py
+# before running this script
+
+rm -rf ../dist
 echo "Release script"
 source ../*env/bin/activate
 poetry build

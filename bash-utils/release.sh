f73979359d68204ec9f495e0cc43b66825c3f187
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2024-01-07 12:14:41 -0500

    Minor fixes in language
    Signed-off-by: NaveenKumar Namachivayam <catch.nkn@gmail.com>

--- bash-utils/release.sh
+++ bash-utils/release.sh
@@ -4,8 +4,20 @@
 # Change the version in pyproject.toml and __version__.py
 # before running this script
 
-rm -rf ../dist
-echo "Release script"
-source ../*env/bin/activate
-poetry build
-twine upload ../dist/*
+# get input from user
+# shellcheck disable=SC2162
+
+read -p "Did you change the version in pyproject.toml and __version__.py " message
+
+if [ "$message" != "y" ]; then
+    echo "Please change the version in pyproject.toml and __version__.py"
+    exit 1
+else
+    echo "Continuing with the release"
+    rm -rf ../dist
+    echo "Release script"
+    source ../*env/bin/activate
+    poetry build
+    twine upload ../dist/*
+
+fi
